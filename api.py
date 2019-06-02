69eda1be7484b8a38e7ec83376f0b7faee7850e6
Huang Junye <h.jun.ye@gmail.com>
2019-06-02 20:54:09 +0800

    Add get_statevector in api
    Not working yet.

--- api.py
+++ api.py
@@ -1,9 +1,18 @@
 from qiskit import QuantumRegister, ClassicalRegister
 from qiskit import QuantumCircuit, Aer, execute
 
-def run_qasm(qasm, backend_to_run="qasm_simulator"):
-    qc = QuantumCircuit.from_qasm_str(qasm)
+
+def run_qasm(qasm, backend_to_run='qasm_simulator'):
+    circuit = QuantumCircuit.from_qasm_str(qasm)
     backend = Aer.get_backend(backend_to_run)
-    job_sim = execute(qc, backend)
-    sim_result = job_sim.result()
-    return sim_result.get_counts(qc)
+    job_sim = execute(circuit, backend)
+    result_sim = job_sim.result()
+    return result_sim.get_counts(circuit)
+
+
+def get_statevector(qasm, backend_to_run='statevector_simulator'):
+    circuit = QuantumCircuit.from_qasm_str(qasm)
+    backend = Aer.get_backend(backend_to_run)
+    job_sim = execute(circuit, backend)
+    result_sim = job_sim.result()
+    return result_sim.get_statevector(circuit, decimals=3)
--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request
-from api import run_qasm
+from api import run_qasm, get_statevector
 import json
 
 app = Flask(__name__)
@@ -31,6 +31,19 @@ def qasm():
     print(qasm)
     print(backend)
     output = run_qasm(qasm, backend)
+    ret = {"result": output}
+    return jsonify(ret)
+
+
+@app.route('/api/run/statevector', methods=['POST'])
+def statevector():
+    data = request.get_json()
+    qasm = data['qasm']
+    backend = data['backend']
+    print("--------------")
+    print(qasm)
+    print(backend)
+    output = get_statevector(qasm, backend)
     ret = {"result": output}
     return jsonify(ret)
 
