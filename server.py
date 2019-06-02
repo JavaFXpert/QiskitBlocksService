e1524a78ba24ac8ba8574e7f13ace60b2894862f
Huang Junye <h.jun.ye@gmail.com>
2019-06-02 16:09:34 +0800

    modify qasm to get from JSON instead of formData

--- server.py
+++ server.py
@@ -4,25 +4,35 @@ import json
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def welcome():
     return "Hi Qiskiter!"
 
-@app.route('/test')
-def test():
-    return "Testing!"
 
-@app.route('/api/run/qasm', methods=['POST'])
-def qasm():
-    qasm = request.form.get('qasm')
+@app.route('/test', methods=['POST'])
+def test_json():
+    data = request.get_json()
+    qasm = data['qasm']
     print("--------------")
-    print (qasm)
-    print(request.get_data())
-    print (request.form)
+    print(qasm)
     backend = 'qasm_simulator'
     output = run_qasm(qasm, backend)
     ret = {"result": output}
     return jsonify(ret)
+
+
+@app.route('/api/run/qasm', methods=['POST'])
+def qasm():
+    data = request.get_json()
+    qasm = data['qasm']
+    print("--------------")
+    print(qasm)
+    backend = 'qasm_simulator'
+    output = run_qasm(qasm, backend)
+    ret = {"result": output}
+    return jsonify(ret)
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000)
